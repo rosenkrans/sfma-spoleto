205a2844461a6a05137a4ea1b9c6abdf2d0f6e24
Mark Rosenkrans <mark.rosenkrans@gmail.com>
2020-01-16 17:26:40 -0500

    updating location of files

--- src/components/SignUpLogInForm.js
+++ src/components/SignUpLogInForm.js
@@ -1,70 +1,70 @@
-// import React from 'react';
-// import { AuthConsumer } from './AuthContext';
+import React from 'react';
+import { AuthConsumer } from './AuthContext';
 
-// class SignUpLogInForm extends React.Component {
-//   emailInput = React.createRef()
-//   passwordInput = React.createRef()
+class SignUpLogInForm extends React.Component {
+  emailInput = React.createRef()
+  passwordInput = React.createRef()
 
-//   redirect = (userId) => {
-//     this.props.history.push(`/${userId}/boards`)
-//   }
+  redirect = (userId) => {
+    this.props.history.push(`/${userId}/boards`)
+  }
 
-//   render() {
-//     return (
-//       <AuthConsumer>
-//         {({signUp, logIn, user, authMessage}) => (
-//           <React.Fragment>
-//             {!user.id ? (
-//               <div className='sign-up-wrapper'>
-//                 <h2>Sign In or Create Account</h2>
-//                 {authMessage ? <span>{authMessage}</span> : ''}
-//                 <form className='sign-up-form'>
-//                   <div>
-//                     <input
-//                       ref={this.emailInput}
-//                       type='email'
-//                       name='email'
-//                       placeholder='Email'
-//                     />
-//                   </div>
-//                   <div>
-//                     <input
-//                       ref={this.passwordInput}
-//                       type='password'
-//                       name='password'
-//                       placeholder='Password'
-//                     />
-//                   </div>
-//                   <div>
-//                     <button 
-//                       onClick={(e) => logIn(
-//                         this.emailInput.current.value,
-//                         this.passwordInput.current.value,
-//                         e
-//                       )}
-//                     >
-//                       Login
-//                     </button>
-//                     <button 
-//                       onClick={(e) => signUp(
-//                         this.emailInput.current.value,
-//                         this.passwordInput.current.value,
-//                         e
-//                       )}
-//                     >
-//                       Sign Up
-//                     </button>
-//                   </div>
-//                 </form>
-//               </div>
-//             ) : (
-//               <button onClick={() => this.redirect(user.id)}>Go to My Boards</button>
-//             )}
-//           </React.Fragment>
-//         )}
-//       </AuthConsumer>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <AuthConsumer>
+        {({signUp, logIn, user, authMessage}) => (
+          <React.Fragment>
+            {!user.id ? (
+              <div className='sign-up-wrapper'>
+                <h2>Sign In or Create Account</h2>
+                {authMessage ? <span>{authMessage}</span> : ''}
+                <form className='sign-up-form'>
+                  <div>
+                    <input
+                      ref={this.emailInput}
+                      type='email'
+                      name='email'
+                      placeholder='Email'
+                    />
+                  </div>
+                  <div>
+                    <input
+                      ref={this.passwordInput}
+                      type='password'
+                      name='password'
+                      placeholder='Password'
+                    />
+                  </div>
+                  <div>
+                    <button 
+                      onClick={(e) => logIn(
+                        this.emailInput.current.value,
+                        this.passwordInput.current.value,
+                        e
+                      )}
+                    >
+                      Login
+                    </button>
+                    <button 
+                      onClick={(e) => signUp(
+                        this.emailInput.current.value,
+                        this.passwordInput.current.value,
+                        e
+                      )}
+                    >
+                      Sign Up
+                    </button>
+                  </div>
+                </form>
+              </div>
+            ) : (
+              <button onClick={() => this.redirect(user.id)}>Go to My Boards</button>
+            )}
+          </React.Fragment>
+        )}
+      </AuthConsumer>
+    )
+  }
+}
 
-// export default SignUpLogInForm;
+export default SignUpLogInForm;
