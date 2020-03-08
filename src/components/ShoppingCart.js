a8d83ff36f81494a9a3c7badefa4e0a7fe78c030
Mark Rosenkrans <mark.rosenkrans@gmail.com>
2020-03-08 13:46:48 -0400

    beginning to work on shopping cart layout

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -23,6 +23,22 @@ class ShoppingCart extends React.Component {
     
   // }
 
+  // getCartData = async userId => {
+  //   try{
+  //     const cartData = await yourpeopleRef
+  //     .where('aboutUser.userId', '==', userId)
+  //     .get()
+  //     console.log(personData)
+  //     const peopleNames = [
+  //       ...personData.docs[0].data().aboutUser.adults.map(person => ({name: person.name, roomType: ''})),
+  //       ...personData.docs[0].data().aboutUser.minors.map(person => ({name: person.name, roomType: ''}))
+  //     ]
+  //     this.setState({people: peopleNames})
+  //   } catch(error){
+  //     console.log('Error getting personData', error)
+  //   }
+  // }
+
   handleInputChange = (e) => {
     
   }
@@ -43,7 +59,17 @@ class ShoppingCart extends React.Component {
         </div>
 
         <form onSubmit={this.handleSubmit} className="form">
-     
+
+          <div>
+            <h3>Please check your information below</h3>
+          </div>
+
+          <div>
+            <p>Number of Type A Rooms: X $__</p>
+            <p>Number of Type B Rooms: X $__</p>
+            <p>Number of Parking Spots: X $__</p>
+            <p>Here is your total: $__</p>
+          </div>
         
           <button 
             onClick={this.handleSubmit}
